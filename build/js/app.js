0127081d0c4697158702780570ebca11426d9e06
Pedro2804 <pedro28hdez@hotmail.com>
2024-06-01 10:34:12 -0600

    Vídeo 198. Agregando navegación fija.

--- build/js/app.js
+++ build/js/app.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+    navegacionFija()
     crearGaleria()
 })
 
-function crearGaleria(){
+function navegacionFija(){
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
 
+    document.addEventListener('scroll', function(){
+        if(sobreFestival.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed')
+        }
+        else{
+            header.classList.remove('fixed')
+        }
+    })
+}
+
+function crearGaleria(){
     const CANTIDAD_IMAGENES = 16
     const galeria = document.querySelector('.galeria-imagenes')
 
@@ -22,20 +36,39 @@ function crearGaleria(){
 }
 
 function mostrarImagen(i){
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galería'
+
     //Generla Modal
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
     modal.onclick = cerrarModal
 
+    //Botón cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
     //Agregar al HTML
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
     body.appendChild(modal)
-
 }
 
 function cerrarModal(){
     const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
 
-    modal?.remove()
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500);
 }
 
